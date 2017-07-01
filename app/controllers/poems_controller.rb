1dfd18b7a4a02e865e8c640a936c9a25958ad533
Luisa Scavo <luisascavo@gmail.com>
2017-07-01 14:29:52 -0400

    add functionality for showing individual poems

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -3,4 +3,10 @@ class PoemsController < ApplicationController
   get "/poems" do
     erb :"/poems/poems"
   end
+
+  get "/poems/:id" do
+    @poem = Poem.find_by_id(params[:id])
+    @author = @poem.user
+    erb :"/poems/show"
+  end
 end
