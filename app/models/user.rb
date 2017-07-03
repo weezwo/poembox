9a35d5a540d749fd6538e736392faa149ea6ee40
Luisa Scavo <luisascavo@gmail.com>
2017-07-03 11:39:24 -0400

    add slug methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
   has_many :poems
   has_many :ratings
   has_secure_password
+
+  def slug
+    self.name.downcase.split.join("-")
+  end
+
+  def find_by_slug(slug)
+    unslug = slug.split("-").join(" ")
+    self.where('lower(name) = ?', unslug).first
+  end
 end
