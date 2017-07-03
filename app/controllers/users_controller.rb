639f0069102c045463648adc75a7e7f8aef031af
Luisa Scavo <luisascavo@gmail.com>
2017-07-03 14:02:39 -0400

    fix user finding in /users/:slug controller method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,8 +42,8 @@ class UsersController < ApplicationController
 
   get '/users/:slug' do
     if signed_in?
-      @user = User.find_by_slug(params[:slug])
-      erb
+      @author = User.find_by_slug(params[:slug])
+      erb :"users/show"
     end
   end
 end
