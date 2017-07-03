a5e5701421690dcbb320e48b3d0d1b542d604fa8
Luisa Scavo <luisascavo@gmail.com>
2017-07-03 11:39:55 -0400

    add ratings controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,16 @@
 class RatingsController < ApplicationController
+  post '/ratings/:poem_id' do
+    if signed_in?
+      rating = Rating.find_or_create_by(user_id: current_user.id, poem_id: params[:poem_id])
+      case params[:rating]
+      when "up"
+        rating.value = 1
+        rating.save
+      when "down"
+        rating.value = -1
+        rating.save
+      end
+    end
+    redirect back
+  end
 end
