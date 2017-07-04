7b86310b327ec39ed07309a56607e7f118ead979
Luisa Scavo <luisascavo@gmail.com>
2017-07-04 13:58:52 -0400

    implement recaptcha

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+require 'json'
 class PoemsController < ApplicationController
 
   get "/poems" do
@@ -23,19 +25,34 @@ class PoemsController < ApplicationController
   end
 
   post "/poems" do
-    if !params["content"].empty?
-      poem = Poem.create(params)
-      poem.user = current_user
-      if params[:title].empty?
-        poem.title = "Untitled"
+    res = Net::HTTP.post_form(
+    URI.parse('http://www.google.com/recaptcha/api/verify'),
+    {
+      'privatekey' => '6Ldt5icUAAAAADMgPkDRpb5S3sZWvDoSH0Va7Dax',
+      'remoteip'   => request.ip,
+      'challenge'  => params[:recaptcha_challenge_field],
+      'response'   => params[:recaptcha_response_field]
+    }
+    )
+
+    success, error_key = res.body.lines.map(&:chomp)
+
+    if success == 'true'
+      if !params["content"].empty?
+        poem = Poem.create(params)
+        poem.user = current_user
+        if params[:title].empty?
+          poem.title = "Untitled"
+        end
+        poem.save
+        redirect("/poems/#{poem.id}")
+      else
+        redirect back
       end
-      poem.save
-      redirect("/poems/#{poem.id}")
     else
       redirect back
     end
   end
-
   get "/poems/:id" do
     @poem = Poem.find_by_id(params[:id])
     if @poem
