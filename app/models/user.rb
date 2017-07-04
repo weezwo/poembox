0f5a33e41866afa3c1e98dc03f6149a30752518d
Luisa Scavo <luisascavo@gmail.com>
2017-07-04 12:46:54 -0400

    fix slug method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def slug
-    self.name.downcase.split.join("-")
+    self.username.downcase.split.join("-")
   end
 
   def self.find_by_slug(slug)
